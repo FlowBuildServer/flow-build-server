2519ceda1f3f339ab7e02afff8d7a011fb5244fe
devMauris <devmauris@gmail.com>
2017-03-18 12:16:58 +0400

    Adding mux

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/docker/docker/client"
-	"github.com/google/go-github/github"
-	"github.com/levigross/grequests"
-	"gopkg.in/yaml.v2"
+	"github.com/gorilla/mux"
+	//"github.com/docker/docker/client"
+	//"github.com/google/go-github/github"
+	//"github.com/levigross/grequests"
+	//"gopkg.in/yaml.v2"
 )
 
 func main() {
-	fmt.Print("Kill Jenkins")
+
 }
