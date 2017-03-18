2581d280159de39e403b84c7547f8eaaa8b64f75
Dmitry Landa <dmitry.landa@yandex.ru>
2017-03-18 14:16:41 +0400

    Improve github puller

--- puller.go
+++ puller.go
@@ -3,11 +3,13 @@ package main
 import (
     "context"
     "github.com/google/go-github/github"
+    "strings"
+    "errors"
     //"fmt"
 )
 
 type Puller struct {
-    //FetchRecentPullRequests func()
+    RepoLink string
 }
 
 type Creds struct {
@@ -15,14 +17,32 @@ type Creds struct {
     Repo string
 }
 
-func (p *Puller) FetchRecentPullRequests(creds *Creds) ([]*github.PullRequest, error) {
+func (p *Puller) validate() (error) {
+    //check if it is github repo
+    if !strings.Contains(p.RepoLink, "github.com") {
+        return errors.New("Github only!")
+    }
+
+    return nil
+}
+
+func (p *Puller) run() ([]*github.PullRequest, error) {
+    //build creds (silly way - need to improve)
+    parts := strings.Split(p.RepoLink, "/")
+    creds := Creds{parts[len(parts) - 2], parts[len(parts) - 1]}
+    //fetch pulls
+    return fetchRecentPullRequests(&creds)
+}
+
+func fetchRecentPullRequests(creds *Creds) ([]*github.PullRequest, error) {
     var allPullRequests []*github.PullRequest
     ctx := context.Background()
     client := github.NewClient(nil)
     options := &github.PullRequestListOptions{
         State: "open",
-        ListOptions: github.ListOptions{PerPage: 5},
+        ListOptions: github.ListOptions{PerPage: 50},
     }
+
     // iterate though pages to fetch'em all
     for {
         pullRequests, resp, error := client.PullRequests.List(
@@ -33,13 +53,29 @@ func (p *Puller) FetchRecentPullRequests(creds *Creds) ([]*github.PullRequest, e
         )
         if error != nil {
             return nil, error
+        } else {
+            allPullRequests = append(allPullRequests, pullRequests...)
+            if resp.NextPage == 0 {
+                break
+            }
+            options.ListOptions.Page = resp.NextPage
         }
-        allPullRequests = append(allPullRequests, pullRequests...)
-        if resp.NextPage == 0 {
-            break
-        }
-        options.ListOptions.Page = resp.NextPage
     }
 
     return allPullRequests, nil
+}
+
+func fetchRepo(creds *Creds) (*github.Repository, error) {
+    ctx := context.Background()
+    client := github.NewClient(nil)
+    repo, _, error := client.Repositories.Get(
+        ctx,
+        creds.Owner,
+        creds.Repo,
+    )
+    if error != nil {
+        return nil, error
+    }
+
+    return repo, nil
 }
