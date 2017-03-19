52a20865cf2237632a79a8ffa32efaabe5fccbf5
Dmitry Landa <dmitry.landa@yandex.ru>
2017-03-19 04:26:24 +0400

    Refactor code to validate url before go routine run, add http responses to api

--- flowrest.go
+++ flowrest.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
@@ -13,13 +13,14 @@ import (
 )
 
 var pipechans chan int
-var pipes []Pipe
+var pipes []*Pipe
 
 type pipe_post struct {
 	URL string
 }
 
 func flowbuild(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 	w.Write([]byte("Jenkins killa\n"))
 }
 
@@ -29,10 +30,22 @@ func addnewpipe(w http.ResponseWriter, r *http.Request) {
 	var in pipe_post
 	err := decoder.Decode(&in)
 	if err != nil {
-		log.Println("Malformed request", err)
+		w.WriteHeader(400)
+		w.Write([]byte("{\"message\": \"Malformed request\"}"))
+		return
 	}
+
 	log.Println(in)
-	pipes = append(pipes, NewPipe(in.URL))
+	pipe, err := NewPipe(in.URL)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf("{\"message\": \"%v\"}", err)))
+		return
+	} else {
+		pipes = append(pipes, pipe)
+		w.WriteHeader(200)
+		w.Write([]byte("{\"message\": \"OK\"}"))
+	}
 }
 func waitforpipes() {
 	<-pipechans
@@ -42,7 +55,7 @@ func waitforpipes() {
 
 func StartFlowBuilder() {
 	defer waitforpipes()
-	pipes = make([]Pipe, 20)
+	pipes = make([]*Pipe, 20)
 	r := mux.NewRouter()
 	r.HandleFunc("/", flowbuild).Methods("GET")
 	r.HandleFunc("/newpipe", addnewpipe).Methods("POST")
